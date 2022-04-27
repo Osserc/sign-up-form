0ea9878d67353c52c7c45dffc0ec8a9fad9704c5
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-27 18:18:04 +0200

    Conclude project

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,12 @@
-const password_one = document.getElementById("password_one")
-const password_two = document.getElementById("password_two")
+/* I already started writing a function before realizing it wasn't necessary
+Leaving it here in case I revisit this project */
 
+function matchPassword() {
+    const password_one = document.getElementById("password_one")
+    const password_two = document.getElementById("password_two")
+
+    if (password_one != password_two) {
+    document.getElementByClassName("message").innerHTML = "*Passwords do not match";  
+    }
+
+}
